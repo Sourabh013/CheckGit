b46695496a71ddfaf1476d702604c30365ec5f9c
Sourabh Modi <modisourabh31@gmail.com>
2024-07-09 13:08:23 +0530

    CCCCCC

--- ChekGit/Checking.swift
+++ ChekGit/Checking.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import UIKit
+
+class Checking: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ChekGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChekGit
+//
+//  Created by Sourabh Modi on 09/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("FDfdfdf")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
